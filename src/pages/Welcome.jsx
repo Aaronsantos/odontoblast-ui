a5eff13a6362f287a093fc38f8edbf23a8fd5cb3
Aaron Santos <aaronpedro16@gmail.com>
2020-06-16 19:31:16 -0300

    Teste webhook de novo .-.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,12 +6,7 @@ export default () => (
       style={{
         textAlign: 'center',
       }}
-    >
-      
-      
-      
-      
-      
+    > 
       Want to add more pages? Please refer to{' '}
       <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
         use block
